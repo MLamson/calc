c0044d66433ce5487c55b27c6a25d29ae6e284be
Mark Lamson <lamson_mark@yahoo.com>
2015-01-14 14:46:23 -0500

    before scss

--- main.js
+++ main.js
@@ -1,23 +1,52 @@
 //Declare vars
-var num_one, num_two, answer, calculateBtn;
+var num_one, num_two, answer, add, subtract;
 var num_one_value, num_two_value, answer_value;
-
+var multiply, divide;
 //set up my vars
 num_one = document.getElementById('num_one');
 num_two = document.getElementById('num_two');
 answer = document.getElementById('answer');
-calculateBtn = document.getElementById('calculateBtn');
+add = document.getElementById('add');
+subtract = document.getElementById('subtract');
+multiply = document.getElementById('multiply');
+divide = document.getElementById('divide');
 
-calculateBtn.onclick = function(){
+add.onclick = function(){
 	num_one_value = num_one.value;
 	num_two_value = num_two.value;
 	num_one_value = Number(num_one_value);
 	num_two_value = Number(num_two_value);
-
-	answer_value = num_one_value + num_two_value;
+	if (isNaN(num_one_value)){
+	console.log('not a number')
+} else {answer_value = num_one_value + num_two_value;
   answer.innerHTML = answer_value;
+  }
 }
 
+subtract.onclick = function(){
+	num_one_value = num_one.value;
+	num_two_value = num_two.value;
+	num_one_value = Number(num_one_value);
+	num_two_value = Number(num_two_value);
+	answer_value = num_one_value - num_two_value;
+  answer.innerHTML = answer_value;
+}
+multiply.onclick = function(){
+	num_one_value = num_one.value;
+	num_two_value = num_two.value;
+	num_one_value = Number(num_one_value);
+	num_two_value = Number(num_two_value);
+	answer_value = num_one_value * num_two_value;
+  answer.innerHTML = answer_value;
+}
+divide.onclick = function(){
+	num_one_value = num_one.value;
+	num_two_value = num_two.value;
+	num_one_value = Number(num_one_value);
+	num_two_value = Number(num_two_value);
+	answer_value = num_one_value / num_two_value;
+  answer.innerHTML = answer_value;
+}
 
 
 
